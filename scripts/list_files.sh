d31a6b205fa2a79e9ee71488b1cd86bf5249e8e2
svobodamichal <svoboda.michalg@gmail.com>
2023-09-12 15:07:26 +0200

    File lists 2

--- scripts/list_files.sh
+++ scripts/list_files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Check if at least one directory path is provided
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <output_file> <directory1> [directory2] [directory3] ..."
+# Check if the user provided a directory path as an argument
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <output_file> [directory1] [directory2] [directory3] ..."
     exit 1
 fi
 
 output_file="$1"
 shift   # Remove the first argument (output_file) from the list of arguments
 
-# Loop through the remaining arguments, which are directory paths
+# Loop through the remaining arguments, which are directory names
 for directory in "$@"; do
     # Use the 'find' command to list all files in the specified directory and append to the output file
     find "$directory" -type f >> "$output_file"
