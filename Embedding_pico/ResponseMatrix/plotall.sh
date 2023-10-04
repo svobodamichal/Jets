deafca08b0981e93b4d073805fe32f3c8434a318
svobodamichal <svoboda.michalg@gmail.com>
2023-10-04 16:20:07 +0200

    Unfolding 2

---
+++ Embedding_pico/ResponseMatrix/plotall.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+rm pythia6_all.root
+
+PTHARD=(3 5 7 9 11 15 20 25 30 40 50 -1)
+
+for (( i=0; i<${#PTHARD[@]}-1 ; i+=1 ))
+do
+PTMIN="${PTHARD[i]}"
+PTMAX="${PTHARD[i+1]}"
+
+root 'plothisto.cxx("'${PTMIN}'_'${PTMAX}'")' -b -q
+
+done #pThard bins
+
+hadd pythia6_all.root pythia6_normalized_*.root 
+
+root -b -q ploteffi.cxx
+
+mv effi* plots_effi
