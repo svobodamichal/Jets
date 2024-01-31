474fef5afd0f0ab2104f390de20b45f7a74c706a
svobodamichal <svoboda.michalg@gmail.com>
2024-01-31 14:04:18 +0100

    test

--- Embedding_pico/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
+++ Embedding_pico/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
@@ -1166,7 +1166,7 @@ int StPicoHFJetMaker::MakeJets() {
 
 			cout << "Eta MC  " << MCmatchedeta << "Eta RC " << RCmatchedeta << endl;
 			cout << "Phi MC  " << MCmatchedphi << "Phi RC " << RCmatchedphi << endl;
-			cout << "pT  "<< pT_corr_det<<endl;
+			cout << "pT  MC  " << pT_true  << "pT RC  " << pT_det <<endl;
 
 
 			static_cast<TH2D*>(mOutList->FindObject(Form("hetaphi_MCmatched_R0%.0lf_centbin%i",fR[i]*10, centrality)))->Fill(MCmatchedeta,MCmatchedphi, weight);
