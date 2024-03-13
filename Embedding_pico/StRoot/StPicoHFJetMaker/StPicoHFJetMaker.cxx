e21dbbfe49837daa8843b1f854b6315c1534e483
svobodamichal <svoboda.michalg@gmail.com>
2024-03-13 16:00:05 +0100

    Test

--- Embedding_pico/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
+++ Embedding_pico/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
@@ -917,8 +917,8 @@ int StPicoHFJetMaker::MakeJets() {
                     static_cast<TH1D*>(mOutList->FindObject(Form("heta_MCRCw_R0%.0lf", fR[i]*10)))->Fill(value, weight);
 
                 }
-/*
-                for (size_t i = 0; i < differEta.size(); ++i) {
+
+                for (int i = 0; i < differEta.size(); ++i) {
                     double etaValue = differEta[i];
                     double phiValue = differPhi[i];
 
@@ -926,7 +926,7 @@ int StPicoHFJetMaker::MakeJets() {
                     static_cast<TH2D*>(mOutList->FindObject(Form("hEtaPhi_MC-RCw_R0%.0lf", fR[i]*10)))->Fill(etaValue, phiValue + TMath::Pi(), weight);
 
                 }
-
+/*
                 cout<< "Test 2" << endl;
                 for (int j = 0; j < deltaR.size(); ++j) {
                     double deltaRvalue = deltaR[j].first;
