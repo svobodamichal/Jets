f1024f3a976c4cd20e59b5a4940e05fef8bde750
svobodamichal <svoboda.michalg@gmail.com>
2024-10-18 23:05:54 +0200

    Trigger study 1.6

--- TriggerStudy/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
+++ TriggerStudy/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
@@ -128,6 +128,10 @@ int StPicoHFJetMaker::InitJets() {
     	mOutList->Add(new TH2D("hdca_pT", "track DCA vs. p_{T}", 90, 0, 3, npttrackbins, pttrackmin, pttrackmax));
 	mOutList->Add(new TH1D("hcharged_tr", "track charge", 90, 0, 3));
 
+    mOutList->Add(new TH1D("hpT_tr_HT1", "track pT; p_{T} [GeV/c]", npttrackbins, pttrackmin, pttrackmax));
+    mOutList->Add(new TH1D("hET_tow_HT1", "tower ET; E_{T} (GeV)", npttrackbins, pttrackmin, pttrackmax));
+
+
     for (int centbin = 1; centbin < 8; centbin++) {
         TString hname = Form("hPrimTrackMB_centbin%i", centbin);
         mOutList->Add(new TH1D(hname, "primary track p_{T} for MB; p_{T} [GeV/c]", npttrackbins, pttrackmin, pttrackmax));
@@ -461,6 +465,9 @@ int StPicoHFJetMaker::MakeJets() {
             static_cast<TH1D*>(mOutList->FindObject(Form("hPrimTowerHT3_centbin%i",centrality)))->Fill(ET, weight);
         }
 
+        if((picoEvent->isTrigger(450201) || picoEvent->isTrigger(450211)&&picoEvent->!isTrigger(450202) || picoEvent->!isTrigger(450212)){
+            static_cast<TH1D*>(mOutList->FindObject("hET_tow_HT1"))->Fill(ET, weight);
+        }
 
 		//no clustering
 		double px,py,pz;
@@ -516,7 +523,9 @@ int StPicoHFJetMaker::MakeJets() {
         if(picoEvent->isTrigger(450203) || picoEvent->isTrigger(450213)){
             static_cast<TH1D*>(mOutList->FindObject(Form("hPrimTrackHT3_centbin%i",centrality)))->Fill(pT, weight);
         }
-
+        if((picoEvent->isTrigger(450201) || picoEvent->isTrigger(450211)&&picoEvent->!isTrigger(450202) || picoEvent->!isTrigger(450212)){
+            static_cast<TH1D*>(mOutList->FindObject("hpT_tr_HT1"))->Fill(pT, weight);
+        }
 
         //PseudoJet inputParticle(trk->gMom().x(), trk->gMom().y(), trk->gMom().z(), trk->gMom().Mag());
 				//primary tracks        
