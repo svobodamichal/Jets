f0badeed6bf72830b00519223532acc44f0c7453
svobodamichal <svoboda.michalg@gmail.com>
2024-11-13 15:36:27 +0100

    MB recalculation 1.19

--- AssociatedTriggerJets/StRoot/StPicoJetMaker/StPicoJetMaker.cxx
+++ AssociatedTriggerJets/StRoot/StPicoJetMaker/StPicoJetMaker.cxx
@@ -181,9 +181,11 @@ Int_t StPicoJetMaker::Make() {
           // Calculate the weight using data from both sources
           double weightEVT = calculateWeight(bhtRunData, vpdRunData);
 
+          cout << "Weight: " << weightEVT << endl;
+
           // Fill your histogram with the weight
           static_cast<TH1D*>(mOutList->FindObject("hrunId_weighted"))->Fill(runNumber, weightEVT);
-          static_cast<TH1I*>(mOutList->FindObject("hevents_weighted"))->Fill(1, weightEVT);
+          static_cast<TH1D*>(mOutList->FindObject("hevents_weighted"))->Fill(1, weightEVT);
       } else {
           std::cerr << "Warning: Run number " << runNumber << " not found in one or both run data maps." << std::endl;
       }
@@ -291,7 +293,7 @@ void StPicoJetMaker::initializeEventStats() {
 
 
   mOutList->Add(new TH1D("hrunId_weighted", "minimum bias events", 90913, 15076101, 15167014)); //15076101−15167014
-  mOutList->Add(new TH1I("hevents_weighted", "number of minimum bias events", 2, 0, 2));
+  mOutList->Add(new TH1D("hevents_weighted", "number of minimum bias events", 2, 0, 2));
 
 
   //All event histograms
