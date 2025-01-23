f221c058d2bb9c3251a0c570b5c1bbf7845e713f
svobodamichal <svoboda.michalg@gmail.com>
2025-01-23 15:13:13 +0100

    Jdeme fixovat

--- Embedding_pico/StRoot/StPicoHFJetMaker/JetInfo.h
+++ Embedding_pico/StRoot/StPicoHFJetMaker/JetInfo.h
@@ -26,11 +26,11 @@ Some additional info and functions for the analysis
 #include "TParticlePDG.h"
 #include "TDatime.h"
 
-#include </fastjet1/fastjet_install/include/fastjet/config.h>
-#include </fastjet1/fastjet_install/include/fastjet/PseudoJet.hh>
-#include </fastjet1/fastjet_install/include/fastjet/JetDefinition.hh>
-#include </fastjet1/fastjet_install/include/fastjet/ClusterSequence.hh>
-#include </fastjet1/fastjet_install/include/fastjet/ClusterSequenceArea.hh>
+#include </gpfs01/star/pwg/svomich/Jets/Embedding_pico/fastjet1/fastjet_install/include/fastjet/config.h>
+#include </gpfs01/star/pwg/svomich/Jets/Embedding_pico/fastjet1/fastjet_install/include/fastjet/PseudoJet.hh>
+#include </gpfs01/star/pwg/svomich/Jets/Embedding_pico/fastjet1/fastjet_install/include/fastjet/JetDefinition.hh>
+#include </gpfs01/star/pwg/svomich/Jets/Embedding_pico/fastjet1/fastjet_install/include/fastjet/ClusterSequence.hh>
+#include </gpfs01/star/pwg/svomich/Jets/Embedding_pico/fastjet1/fastjet_install/include/fastjet/ClusterSequenceArea.hh>
 
 
 #include <vector>
