50625121748467ba3a5078e0a4bb70ecc7c4c92b
svobodamichal <svoboda.michalg@gmail.com>
2025-02-12 13:48:25 +0100

    ADC data final

--- AssociatedTriggerJets/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
+++ AssociatedTriggerJets/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
@@ -440,9 +440,8 @@ int StPicoHFJetMaker::MakeJets() {
 		py = ET*sin(Towphi);
 		pz = towE*tanh(Toweta);
 
-        int ADC = towHit->adc();
-        if (ADC>18)
-        cout << "ADC tower loop: " << ADC <<" Energy: " << towE<< endl;
+        int ADC = towHit->adc()>>4;
+    //    cout << "ADC tower loop: " << ADC <<" Energy: " << towE<< endl;
         static_cast<TH1D*>(mOutList->FindObject("hADC"))->Fill(ADC, weight);
 
 		PseudoJet inputTower(px, py, pz, towE);
@@ -979,7 +978,7 @@ Bool_t StPicoHFJetMaker::GetCaloTrackMomentum(StPicoDst *mPicoDst, TVector3 mPri
 		double vz = mPrimVtx.z();
 		float Toweta = vertexCorrectedEta(Toweta_tmp, vz); 
 		float energy = GetTowerCalibEnergy(towid);
-        int ADC = trg->adc();
+        int ADC = trg->adc()>>4;
        // cout<<"Tower ID: "<<towid-1 <<"Tow ADC: "<< ADC<<" Calib Energy: " << energy << endl;
 		if (ADC > fTrgthresh) Triggers.push_back(towid); // This cut used to be on energy level, now trying to use ADC
 	} 	
