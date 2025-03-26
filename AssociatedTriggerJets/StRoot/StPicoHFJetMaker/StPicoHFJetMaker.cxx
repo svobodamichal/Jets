1ea8def6107281f035a2f387843cdd9edaa6a37f
svobodamichal <svoboda.michalg@gmail.com>
2025-03-26 02:25:26 +0100

    Centrality back, track eff

--- AssociatedTriggerJets/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
+++ AssociatedTriggerJets/StRoot/StPicoHFJetMaker/StPicoHFJetMaker.cxx
@@ -403,15 +403,12 @@ int StPicoHFJetMaker::MakeJets() {
     double weightEVT = getWeight(runNumber);
     float WeightTotal = weight * weightEVT; // To arrive to corresponding number of MB events
 
-    cout<<"centrality: "<<centrality<<" weight: "<<weight<<" weightEVT: "<<weightEVT<<" WeightTotal: "<<WeightTotal<<endl;
+//    cout<<"centrality: "<<centrality<<" weight: "<<weight<<" weightEVT: "<<weightEVT<<" WeightTotal: "<<WeightTotal<<endl;
 
     static_cast<TH1D*>(mOutList->FindObject("hEVTcentral"))->Fill(centrality, 1*WeightTotal);
 
     if (centrality == 0) centrality = 1; // merge 0-5% and 5-10% into 0-10%
     if (centrality == 8) centrality = 7; // merge 60-70% and 70-80% into 60-80%
-
-    cout<<"centrality: "<<centrality<<" weight: "<<weight<<" weightEVT: "<<weightEVT<<" WeightTotal: "<<WeightTotal<<endl;
-
 
     static_cast<TH1D*>(mOutList->FindObject("hcent"))->Fill(centrality, weight);
 	//static_cast<TH2D*>(mOutList->FindObject("hrunIdcent"))->Fill(fRunNumber,centrality,weight); //not used
